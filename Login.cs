b7f483bc53d8aeea55d9642acfe2c142af9ea951
yunusabdullaev0707@gmail.com <yunusabdullaev0707@gmail.com>
2024-11-11 11:12:45 +0500

    add windows

---
+++ Login.cs
@@ -0,0 +1,41 @@
+namespace CRMStom
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show(this);
+        }
+    }
+}
---
+++ MainMenu.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace CRMStom
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            groupBox4 = new GroupBox();
+            textBox8 = new TextBox();
+            label11 = new Label();
+            textBox7 = new TextBox();
+            label10 = new Label();
+            textBox6 = new TextBox();
+            label9 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            label8 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            label7 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label6 = new Label();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox3 = new GroupBox();
+            groupBox2.SuspendLayout();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(0, -4);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(212, 826);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.BackColor = SystemColors.AppWorkspace;
+            groupBox2.Controls.Add(groupBox4);
+            groupBox2.Location = new Point(218, 58);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(1158, 826);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            groupBox4.BackColor = Color.White;
+            groupBox4.Controls.Add(textBox8);
+            groupBox4.Controls.Add(label11);
+            groupBox4.Controls.Add(textBox7);
+            groupBox4.Controls.Add(label10);
+            groupBox4.Controls.Add(textBox6);
+            groupBox4.Controls.Add(label9);
+            groupBox4.Controls.Add(textBox5);
+            groupBox4.Controls.Add(label5);
+            groupBox4.Controls.Add(label8);
+            groupBox4.Controls.Add(dateTimePicker2);
+            groupBox4.Controls.Add(label7);
+            groupBox4.Controls.Add(dateTimePicker1);
+            groupBox4.Controls.Add(label6);
+            groupBox4.Controls.Add(textBox4);
+            groupBox4.Controls.Add(textBox3);
+            groupBox4.Controls.Add(label4);
+            groupBox4.Controls.Add(textBox2);
+            groupBox4.Controls.Add(label3);
+            groupBox4.Controls.Add(textBox1);
+            groupBox4.Controls.Add(label2);
+            groupBox4.Controls.Add(label1);
+            groupBox4.Location = new Point(16, 22);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(1123, 340);
+            groupBox4.TabIndex = 0;
+            groupBox4.TabStop = false;
+            groupBox4.Enter += groupBox4_Enter;
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox8.Location = new Point(867, 102);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(239, 29);
+            textBox8.TabIndex = 22;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.BackColor = Color.White;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label11.ForeColor = Color.FromArgb(0, 192, 192);
+            label11.Location = new Point(878, 68);
+            label11.Name = "label11";
+            label11.Size = new Size(154, 21);
+            label11.TabIndex = 21;
+            label11.Text = "Сумма со скидкой";
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox7.Location = new Point(589, 272);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(239, 29);
+            textBox7.TabIndex = 20;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.White;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label10.ForeColor = Color.FromArgb(0, 192, 192);
+            label10.Location = new Point(600, 238);
+            label10.Name = "label10";
+            label10.Size = new Size(194, 21);
+            label10.TabIndex = 19;
+            label10.Text = "Информация о приёме";
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox6.Location = new Point(589, 187);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(239, 29);
+            textBox6.TabIndex = 18;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.White;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label9.ForeColor = Color.FromArgb(0, 192, 192);
+            label9.Location = new Point(600, 153);
+            label9.Name = "label9";
+            label9.Size = new Size(150, 21);
+            label9.TabIndex = 17;
+            label9.Text = "Тип визита-бронь";
+            label9.Click += label9_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox5.Location = new Point(589, 102);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(239, 29);
+            textBox5.TabIndex = 16;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.White;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.ForeColor = Color.FromArgb(0, 192, 192);
+            label5.Location = new Point(600, 68);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 21);
+            label5.TabIndex = 15;
+            label5.Text = "Скидка";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.White;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label8.ForeColor = Color.FromArgb(0, 192, 192);
+            label8.Location = new Point(322, 68);
+            label8.Name = "label8";
+            label8.Size = new Size(92, 21);
+            label8.TabIndex = 14;
+            label8.Text = "Тел номер";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.CalendarFont = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dateTimePicker2.Location = new Point(27, 104);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(239, 23);
+            dateTimePicker2.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.White;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label7.ForeColor = Color.FromArgb(0, 192, 192);
+            label7.Location = new Point(38, 68);
+            label7.Name = "label7";
+            label7.Size = new Size(114, 21);
+            label7.TabIndex = 12;
+            label7.Text = "День приёма";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CalendarFont = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dateTimePicker1.Location = new Point(27, 283);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(239, 23);
+            dateTimePicker1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.White;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label6.ForeColor = Color.FromArgb(0, 192, 192);
+            label6.Location = new Point(38, 247);
+            label6.Name = "label6";
+            label6.Size = new Size(123, 21);
+            label6.TabIndex = 9;
+            label6.Text = "Год рождения";
+            label6.Click += label6_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox4.Location = new Point(311, 102);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(239, 29);
+            textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox3.Location = new Point(311, 187);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(239, 29);
+            textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.ForeColor = Color.FromArgb(0, 192, 192);
+            label4.Location = new Point(322, 153);
+            label4.Name = "label4";
+            label4.Size = new Size(149, 21);
+            label4.TabIndex = 5;
+            label4.Text = "Врач консультант";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox2.Location = new Point(27, 187);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(239, 29);
+            textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.ForeColor = Color.FromArgb(0, 192, 192);
+            label3.Location = new Point(38, 153);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 21);
+            label3.TabIndex = 3;
+            label3.Text = "Пациент";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(311, 272);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(239, 29);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.ForeColor = Color.FromArgb(0, 192, 192);
+            label2.Location = new Point(322, 238);
+            label2.Name = "label2";
+            label2.Size = new Size(187, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Ноименование услуги";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.ForeColor = Color.Orange;
+            label1.Location = new Point(27, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(170, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Manage Diagnosis";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Location = new Point(218, -4);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(1355, 56);
+            groupBox3.TabIndex = 0;
+            groupBox3.TabStop = false;
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1371, 877);
+            ControlBox = false;
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "MainMenu";
+            RightToLeft = RightToLeft.Yes;
+            Load += MainMenu_Load;
+            groupBox2.ResumeLayout(false);
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox4;
+        private Label label1;
+        private GroupBox groupBox3;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label6;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private DateTimePicker dateTimePicker1;
+        private Label label8;
+        private DateTimePicker dateTimePicker2;
+        private Label label7;
+        private TextBox textBox6;
+        private Label label9;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox7;
+        private Label label10;
+        private TextBox textBox8;
+        private Label label11;
+    }
+}
