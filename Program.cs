4c3df84c2ecb42adf104a43ac9ae842d03010e23
AlCkoNi <Tolibjon2004@yandex.ru>
2024-11-25 11:04:38 +0500

    .

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Time_Table.db;
 using static Time_Table.db.db_finctions;
 namespace Time_Table
 {
-    internal static class Program
+    internal static class Program 
     {
         [DllImport("kernel32.dll")]
         private static extern bool AllocConsole();
@@ -19,26 +20,33 @@ namespace Time_Table
         db_finctions dbf = new db_finctions();
         public async Task add()
         {
-            user_info newUser = new user_info
+            try
             {
-                id = 6,
-                date_to_visit = "2024-12-01",
-                name = "Иван Иванов",
-                date_to_born = "2000-01-01",
-                tel = 123456789,
-                doxtr = 1,
-                tashxis = "Примерный диагноз",
-                skidka = 10,
-                keldi = "Да",
-                obshynarh = "Общая информация"
-            };
-            await dbf.add_user_to_db(newUser);
+                user_info newUser = new user_info
+                {
+                    id = 6,
+                    date_to_visit = "2024-12-01",
+                    name = "Иван Иванов",
+                    date_to_born = "2000-01-01",
+                    tel = 123456789,
+                    doxtr = 1,
+                    tashxis = "Примерный диагноз",
+                    skidka = 10,
+                    keldi = "Да",
+                    obshynarh = "Общая информация"
+                };
+                await dbf.add_user_to_db(newUser);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"General Error: {ex.Message}");
+                Debug.WriteLine(ex.Message); throw;
+            }
         }
         public async Task run_func()
         {
             await add();
             var allUsers = await dbf.SelectAllUsersAsync();
-
             foreach (var user in allUsers)
             {
                 foreach (var column in user)
